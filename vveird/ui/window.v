21a5f223679576d40e65f1003e9ae9f1ff5e9d2c
gaconkzk <gaconkzk@gmail.com>
2019-12-24 15:09:24 +0700

    starting refactoring

--- vveird/ui/window.v
+++ vveird/ui/window.v
@@ -2,9 +2,25 @@ module ui
 
 pub struct Window {
 mut:
-	owner voidptr
-	cfg &glfw.WinCfg
+  owner voidptr
+  cfg &glfw.WinCfg
 pub mut:
-	width int
-	height int
+  width int
+  height int
+}
+
+pub fn make_window() Window {
+  w := Window {
+    width: 800
+    height: 600
+  }
+
+  return w
+}
+
+pub fn (w Window) get_owner<T>() ?T {
+  if (isnil(w.owner)) {
+    return error('Owner doesn\'t existed.')
+  }
+  return T(w.owner)
 }
--- vveird/vveird.v
+++ vveird/vveird.v
@@ -1,15 +1,15 @@
 module vveird
 
 import (
-	cli
-	conf
-	gl
-	gg
-	glfw
-	gx
-	freetype
-	time
-	vveird.ui
+  cli
+  conf
+  // gl
+  // gg
+  glfw
+  // gx
+  // freetype
+  time
+  vveird.ui
 )
 
 pub fn cmd(config conf.Window) cli.Command {
@@ -20,93 +20,117 @@ pub fn cmd(config conf.Window) cli.Command {
     parent: 0
   }
 
-	return wcmd
+  return wcmd
 }
 
 pub fn exec(cmd cli.Command) {
-	println('start a GUI please')
+  // todo - window config from cmd
 
-	ui.start()
-	// todo - window config from cmd
-  mut vveird := &Vveird {
-		main: 0
-		vg: 0
-		ft: 0
-	}
+  vveird := &Vveird{
+    main: 0
+  }
 
-	w := glfw.create_window(glfw.WinCfg {
-		width: 900
-		height: 800
-		borderless: false
-		title: 'VVeird'
-		ptr: vveird
-	})
+  vveird.initialize()
+  // go vveird.loop()
 
-	vveird.main = w
+  vveird.process()
 
-	w.make_context_current()
-	gl.init_glad()
+  // main.register_event()
 
-	// gg config
-	cfg := gg.Cfg {
-		width: 900
-		height: 800
-		font_size: 14
-		use_ortho: true
-		retina: true
-		scale: 2
-		window_user_ptr: 0
-	}
-	vveird.vg = gg.new_context(cfg)
 
-	// font
-	vveird.ft = freetype.new_context(cfg)
+  // mut vveird := &Vveird {
+  // 	main: 0
+  // 	vg: 0
+  // 	ft: 0
+  // }
 
-	w.set_user_ptr(vveird)
+  // glfw.init_glfw()
 
-	go vveird.loop()
+  // w := glfw.create_window(glfw.WinCfg {
+  // 	width: 900
+  // 	height: 800
+  // 	borderless: false
+  // 	title: 'VVeird'
+  // 	ptr: vveird
+  // })
 
-	gl.clear()
-	gl.clear_color(0, 0, 0, 255)
+  // vveird.main = w
 
-	vveird.refresh = true
-	for !vveird.main.should_close() {
-		if vveird.refresh {
-			gl.clear()
-			gl.clear_color(0, 0, 0, 255)
-		}
+  // w.make_context_current()
+  // gl.init_glad()
 
-		vveird.draw()
+  // // gg config
+  // cfg := gg.Cfg {
+  // 	width: 900
+  // 	height: 800
+  // 	font_size: 14
+  // 	use_ortho: true
+  // 	retina: true
+  // 	scale: 2
+  // 	window_user_ptr: 0
+  // }
+  // vveird.vg = gg.new_context(cfg)
 
-		w.swap_buffers()
-		glfw.wait_events()
-	}
+  // // font
+  // vveird.ft = freetype.new_context(cfg)
+
+  // w.set_user_ptr(vveird)
+
+  // go vveird.loop()
+
+  // gl.clear()
+  // gl.clear_color(0, 0, 0, 255)
+
+  // vveird.refresh = true
+  // for !vveird.main.should_close() {
+  // 	if vveird.refresh {
+  // 		gl.clear()
+  // 		gl.clear_color(0, 0, 0, 255)
+  // 	}
+
+  // 	vveird.draw()
+
+  // 	w.swap_buffers()
+  // 	glfw.wait_events()
+  // }
+}
+
+fn (v Vveird) draw() {
+  println('draw')
+
+  time.sleep(5)
+}
+
+fn (v Vveird) process() {
+  for {
+    v.draw()
+  }
 }
 
 pub struct Vveird {
 mut:
-	states States
+  states States
 
-	main &glfw.Window
-	vg &gg.GG
-	ft &freetype.FreeType
+  main &ui.Window
+  // vg &gg.GG
+  // ft &freetype.FreeType
 
-	refresh bool
+  refresh bool
 }
 
 pub struct User {
-	email string
-	full_name string
-	password string
+  email string
+  full_name string
+  password string
 }
 
 pub struct Position {
-	x int
-	y int
+  x int
+  y int
 }
 
 pub struct States {
-	users []User
+  users []User
 }
 
 fn (vveird mut Vveird) loop() {
@@ -120,6 +144,6 @@ fn (vveird mut Vveird) loop() {
 	}
 }
 
-fn (vveird Vveird) draw() {
-	vveird.vg.draw_rect(0, 0, 900, 800, gx.Color {0, 0, 0})
-}
+// fn (vveird Vveird) draw() {
+// 	vveird.vg.draw_rect(0, 0, 900, 800, gx.Color {0, 0, 0})
+// }
